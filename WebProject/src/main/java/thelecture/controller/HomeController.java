8194a447400a826374a2851d066476472e516c20
spectralfox <spectralfox05@gmail.com>
2018-07-21 12:32:34 +0900

    Membercontroller생성

--- WebProject/src/main/java/thelecture/controller/HomeController.java
+++ WebProject/src/main/java/thelecture/controller/HomeController.java
@@ -46,30 +46,6 @@ public class HomeController {
 		System.out.println("tos");
 		return "tos";
 	}
-	@RequestMapping("joinForm.do")
-	public String joinForm() {		
-		System.out.println("joinForm");
-		return "join_form";
-	}
-	@RequestMapping("join.do")
-	public String join() {		
-		System.out.println("join");
-		return "join";
-	}
-	@RequestMapping("loginForm.do")
-	public String loginForm() {		
-		System.out.println("loginForm");
-		return "login_form";
-	}
-	@RequestMapping("login.do")
-	public String login() {		
-		System.out.println("login");
-		return "login";
-	}
-	@RequestMapping("resetPasswd.do")
-	public String resetPasswd() {		
-		System.out.println("resetPasswd");
-		return "reset_passwd";
-	}
+
 	
 }
--- WebProject/src/main/webapp/js/validation.js
+++ WebProject/src/main/webapp/js/validation.js
@@ -1,27 +1,21 @@
 /* 유효성 검사 */
 function validateForm() {
-	var passwd = document.forms["joinform"]["userPasswd"].value;
-	var passwd2 = document.forms["joinform"]["userPasswd2"].value;
+	var password = document.forms["joinform"]["password"].value;
+	var password2 = document.forms["joinform"]["password2"].value;
 	var permit = true;
-	if (passwd.length < 8) { /* 비밀번호 8자 미만일 경우 */
+	if (password.length < 8) { /* 비밀번호 8자 미만일 경우 */
 		$("#isShort").show();
 		permit = false;
 	} else {
 		$("#isShort").hide();
 	}
-	if (passwd != passwd2) { /* 비밀번호&비밀번호 확인 불일치 */
+	if (password != password2) { /* 비밀번호&비밀번호 확인 불일치 */
 		$("#isntDup").show();
 		permit = false;
 	} else {
 		$("#isntDup").hide();
+		permit = false;
 	}
-	permit = false;
 	return permit;
 }
-$(document).ready(function() {
-	$("#agree").click(function() {
-		var agreec = document.getElementById('agree');
-		alert(agreec);
-	});
-});
 /* End 유효성 검사 */
