c8c3206eef834ff4c6ad21e323b1aa8edd81d8e4
spectralfox <spectralfox05@gmail.com>
2018-08-16 14:04:40 +0900

    commit

--- WebProject/src/main/java/thelecture/controller/UnivController.java
+++ WebProject/src/main/java/thelecture/controller/UnivController.java
@@ -18,54 +18,42 @@ public class UnivController {
 	@Autowired
 	private UnivServiceImpl univService;
 
-
 	/**
 	 * 회원 가입하기 위한 form이 있는 곳으로 이동
 	 */
 	@RequestMapping("univ_list.do")
 	public String univ_list(@RequestParam("category") String category, Model model) {
-		List<String> data_univ_list= univService.getUniv_list();
-<<<<<<< HEAD
-=======
-		
-		
->>>>>>> branch 'test' of https://github.com/JanghoPark24/WebProject
+		List<String> data_univ_list = univService.getUniv_list();
 		model.addAttribute("result", data_univ_list);
 		return "content/university/data_univ_list";
 	}
+
 	@RequestMapping("insertNewUniv.do")
 	public String insert_new_univ(UnivBean univ) {
-		
-		
+
 		return "content/university/data_univ_list";
 	}
-	
-<<<<<<< HEAD
+
 	/**
 	 * 대학 리스트 데이터를 json으로 전달함
 	 */
 	@RequestMapping("autocomplete_univ.do")
 	@ResponseBody
-	public List<String> selectUnivList(){
-		List<String> univ_list= univService.getUniv_list();
+	public List<String> selectUnivList() {
+		List<String> univ_list = univService.getUniv_list();
 		return univ_list;
 	}
-=======
-	//새로운 대학교 입력 
-	@RequestMapping(value="insertUniv.do")
+
+	// 새로운 대학교 입력
+	@RequestMapping(value = "insertUniv.do")
 	public String insertUniv(UnivBean university) {
 		System.out.println(university.getUniv_domain());
 		System.out.println(university.getUniv_logo());
 		System.out.println(university.getUniv_url());
 		System.out.println(university.getUniv_domain());
-		int result =univService.insertUniv(university);
-		//0이 성공 1이 실패  -성공시 view화면으로
-		return result == 0? "redirect:insertLectureView.do":"404error//e";
+		int result = univService.insertUniv(university);
+		// 0이 성공 1이 실패 -성공시 view화면으로
+		return result == 0 ? "redirect:insertLectureView.do" : "404error//e";
 	}
-
-	
-	
->>>>>>> branch 'test' of https://github.com/JanghoPark24/WebProject
-
 
 }
