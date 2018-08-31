50dd8ef6c6d0661052ede1d5da283e1416da79da
Administrator <Administrator@WIN-SC2LPOPULHH>
2018-08-31 13:49:00 +0900

    게시판 기능구현 및 디자인 변경

--- WebProject/src/main/java/thelecture/controller/MemberController2.java
+++ WebProject/src/main/java/thelecture/controller/MemberController2.java
@@ -1,9 +1,11 @@
 package thelecture.controller;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.mail.HtmlEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import thelecture.model.MemberBean;
 import thelecture.model.WriteBean;
 import thelecture.service.MemberServiceImpl;
 import thelecture.service.WriteServiceImple;
@@ -39,6 +40,10 @@ public class MemberController2 {
 		model.addAttribute("result", result);
 		return "writeAction";
 	}
+	
+	
+	
+	
 
 	@RequestMapping("wlist.do")	/*리스트 페이지*/
 	public String list(Model model) {
@@ -70,12 +75,11 @@ public class MemberController2 {
 		return "deleteForm";
 	}	*/
 	
-	@RequestMapping("delete.do")	/*게시글 삭제 페이지*/
+/*	@RequestMapping("delete.do")	게시글 삭제 페이지
 	public String delete(@RequestParam("num") int num, String email, Model model,HttpSession session){
 		String session_email = (String) session.getAttribute("email");
 		String grade = (String) session.getAttribute("grade");
-//		WriteBean board = service.read(num);
-//		if (board.getEmail().equals(session_email) || grade.equals("master")){
+
 		if (email.equals(session_email) || grade.equals("master")){
 			int result = service.delete(num);
 			model.addAttribute("result", result);
@@ -83,26 +87,82 @@ public class MemberController2 {
 		} //운영자 권한 또는 이메일이 같을 경우 
 		else {
 			return "isWrongEmail//e";
+		}*/
+	@RequestMapping(value = "wdelete.do")
+	public String wdelete(int board_num, Model model,String email,HttpSession session) {
 		
-		
+		System.out.println(board_num);
+
+		int result = service.wdelete(board_num);
+
+		return result == 0 ? "redirect:wlist.do" : "isWrongEmail//e";
+	}	
+	
+	@RequestMapping(value = "wupdate.do")
+	public String updateQuestion(WriteBean writebean) {
+		System.out.println();
+		System.out.println("1");
+		int result = service.wupdate(writebean);
+		System.out.println("4");
+		if(result >0) {
+			return "error_check/msg";
+		}else {
+			return "error_check/updateFail";
 		}
 		
-		
-		
-		
-			
 
-			
-	
-
-	
-		
-		
-		
-		
-		
-		
-	
 	}
 	
+	
+	
+	
+	
+	
+
+	
+/*@RequestMapping("wsend.do")
+public String send(Model model) {
+
+	Random random = new Random();
+	int a = random.nextInt(100);
+
+	// Mail Server 설정
+	String charSet = "utf-8";
+	String hostSMTP = "smtp.naver.com";
+	String hostSMTPid = "sanctimony@naver.com";
+	String hostSMTPpwd = "qwejrc47tr6w2!"; // 비밀번호 입력해야함
+
+	// 보내는 사람 EMail, 제목, 내용
+	String fromEmail = "sanctimony@naver.com";
+	String fromName = "행복한 부엉이";
+	String subject = "신고결과 검토중 입니다.";
+
+	// 받는 사람 E-Mail 주소
+	String mail = "happybueong@gmail.com";
+
+	try {
+		HtmlEmail email = new HtmlEmail();
+		email.setDebug(true);
+		email.setCharset(charSet);
+		email.setSSL(true);
+		email.setHostName(hostSMTP);
+		email.setSmtpPort(587);
+
+		email.setAuthentication(hostSMTPid, hostSMTPpwd);
+		email.setTLS(true);
+		email.addTo(mail, charSet);
+		email.setFrom(fromEmail, fromName, charSet);
+		email.setSubject(subject);
+		email.setHtmlMsg("<p align = 'center'>Overflow에 오신것을 환영합니다.</p><br>" 
+						 + "<div align='center'> 인증번호 : " + a + "</div>");
+		email.send();
+	} catch (Exception e) {
+		System.out.println(e);
+	}
+	
+	model.addAttribute("result", "good~!!\n 등록된 E-Mail 확인");
+
+	return "result";
+}*/
+
 }
--- WebProject/src/main/webapp/WEB-INF/views/content/university/write_manager.jsp
+++ WebProject/src/main/webapp/WEB-INF/views/content/university/write_manager.jsp
@@ -17,14 +17,10 @@
 			<form method="post" action="write.do">
 				<table class="table table-striped" style="text-align: center; border: 1px solid #dddddd">
 				<thead>
-					<tr>
-						<th colspan="2" style="background-color: #eeeeee; text-align: center;">게시판 글쓰기 양식</th>
-					</tr>
+					<tr><th colspan="2" style="background-color: #eeeeee; text-align: center;">게시판 글쓰기 양식</th></tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td><input type="text" class="form-control" placeholder="글 제목" name="subject" maxlength="50"></td>
-					</tr>
+					<tr><td><input type="text" class="form-control" placeholder="글 제목" name="subject" maxlength="50"></td>	</tr>
 					<tr>
 						<td><textarea class="form-control" placeholder="글 내용" name="content" maxlength="2048" style="height: 350px;"></textarea></td>
 					</tr>
